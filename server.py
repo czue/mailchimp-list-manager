7c5a1ed5275190b4d8ccd24585e55771caafadd5
Cory Zue <czue@dimagi.com>
2017-04-05 13:35:44 +0200

    start hooking up actual form

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, url_for
+import json
+from flask import Flask, render_template, request, Response
+from mailchimp3 import MailChimp
+import config
 
 app = Flask(__name__)
-
-# url_for('static', filename='css/normalize.css')
-# url_for('static', filename='css/skeleton.css')
 
 @app.route('/')
 def hello_world():
     name = 'Cory Zue'
     return render_template('home.html', name=name)
 
+
+@app.route('/api/lists/', methods=['POST'])
+def get_lists():
+    client = _get_mailchimp_client()
+    lists = client.lists.all(get_all=True, fields="lists.name,lists.id")
+    return Response(json.dumps(lists), mimetype='application/json')
+
+
+def _get_mailchimp_client():
+    username = request.form['username'] or config.MAILCHIMP_USERNAME
+    api_key = request.form['api-key'] or config.MAILCHIMP_SECRET_KEY
+    return MailChimp(username, api_key)
